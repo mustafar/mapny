2e0af7400a743ab60aac09f954b066e1067576e3
Mustafa Rizvi <mustafa@Maude.local>
2014-03-02 17:25:41 -0500

    no dev env. do it live.

--- app.js
+++ app.js
@@ -22,11 +22,6 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
 
-// development only
-if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
-}
-
 app.get('/', routes.index);
 app.get('/:map/:zoom/:lat/:lng', routes.indexFull);
 
